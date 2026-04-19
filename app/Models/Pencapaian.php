531102236e99ef3f25c5ceaa24273c0d13053e16
BagusFachfirliH <163153634+BagusFachfirliH@users.noreply.github.com>
2026-04-19 12:42:57 +0700

    Add files via upload

---
+++ app/Models/Pencapaian.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pencapaian extends Model
+{
+    protected $table = 'pencapaian';
+    protected $primaryKey = 'id_pencapaian';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_siswa',
+        'id_badge',
+        'tanggal_diberikan',
+    ];
+
+    public function siswa()
+    {
+        return $this->belongsTo(Siswa::class, 'id_siswa', 'id_siswa');
+    }
+
+    public function badge()
+    {
+        return $this->belongsTo(MasterBadge::class, 'id_badge', 'id_badge');
+    }
+}
