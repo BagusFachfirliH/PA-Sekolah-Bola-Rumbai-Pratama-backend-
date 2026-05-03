fbc63544b0274d2ebdb203edf3ac14d2f9f1218c
BagusFachfirliH <163153634+BagusFachfirliH@users.noreply.github.com>
2026-05-04 02:51:01 +0700

    Add email verification route to API

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+Route::get('/verify-email', [AuthController::class, 'verifyEmail']);
 
 
 
