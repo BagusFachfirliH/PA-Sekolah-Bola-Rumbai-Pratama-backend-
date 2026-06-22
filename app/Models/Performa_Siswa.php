884a86d7342c849166689aba52dec1268f2160d0
BagusFachfirliH <163153634+BagusFachfirliH@users.noreply.github.com>
2026-06-22 12:09:09 +0700

    Update Performa_Siswa.php

--- app/Models/Performa_Siswa.php
+++ app/Models/Performa_Siswa.php
@@ -9,15 +9,17 @@ class Performa_Siswa extends Model
 {
     protected $table = 'performa_siswa';
     protected $primaryKey = 'id_performa';
+    public $incrementing = true;
+    protected $keyType = 'int';
 
     protected $fillable = [
+        'id_jadwal',
+        'id_pelatih',
         'id_siswa',
         'tanggal_penilaian',
         'dribbling',
         'passing',
-        'shooting',
-        'rata_rata',
-        'keterangan'
+        'shooting'
     ];
 
     public $timestamps = false;
@@ -25,5 +27,15 @@ class Performa_Siswa extends Model
     public function siswa()
     {
         return $this->belongsTo(Siswa::class, 'id_siswa');
+    }
+
+    public function jadwal()
+    {
+        return $this->belongsTo(Jadwal_Latihan::class, 'id_jadwal', 'id_jadwal');
+    }
+
+    public function pelatih()
+    {
+        return $this->belongsTo(Pelatih::class, 'id_pelatih', 'id_pelatih');
     }
 }
